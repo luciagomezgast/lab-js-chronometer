3c5e54b1d5490b67ed0978177ee39f29df2927ea
Lu <luciagomezgastaldu@gmail.com>
2023-01-24 17:46:26 +0100

    Lab_Reloj

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+    this.intervalId = setInterval(()=> {
+      if(printTimeCallback) {printTimeCallback()
+        }
+        this.currentTime ++}, 1000
+    )
+  } 
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+   return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value > 9) return value.toString();
+    return "0" + value;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+       let min = this.computeTwoDigitNumber(this.getMinutes())
+       let seg = this.computeTwoDigitNumber(this.getSeconds())
+
+    return `${min}:${seg}`
   }
 }
